565c1ffcd0033a073405839f7f3c7c2b42753f81
uraniodefarinhalol <laterzadavi9@gmail.com>
2026-05-15 17:48:31 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+meme_dict = {
+            "CRINGE": "Algo vergonhoso ou constrangedor",
+            "STALKEAR": "Investigar a vida de alguém online",
+            }
+
+
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+   
